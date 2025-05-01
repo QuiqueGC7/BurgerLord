659c22a37ca5050cfc49aebbd297d9aa2c7f92ed
DiegoLanda26950 <a26950@svalero.com>
2025-05-01 21:59:07 +0200

    Add files via upload

---
+++ js/burgers.js
@@ -0,0 +1,24 @@
+// Cargar los datos del JSON de hamburguesas
+fetch('./json/burgers.json')
+  .then(res => {
+    if (!res.ok) throw new Error("Error de red");
+    return res.json();
+  })
+  .then(data => {
+    const container = document.getElementById('menu-container');
+    let currentCategory = '';
+    data.forEach(item => {
+      if (item.categoria !== currentCategory) {
+        currentCategory = item.categoria;
+        container.innerHTML += `<section class="categoria">Hamburguesas ${currentCategory}</section>`;
+      }
+      container.innerHTML += `
+        <div class="menu-item">
+          <img src="${item.imagen}" alt="${item.nombre}" id="${item.id}">
+          <h3>${item.nombre}</h3>
+          <p>${item.descripcion}</p>
+          <strong>$${item.precio.toFixed(2)}</strong>
+        </div>`;
+    });
+  })
+  .catch(err => console.error("Error al cargar el menú:", err));
